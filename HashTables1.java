d9fb82e326ff1263aac3a9be93464cc222ee0af2
eunifredb <87236384+eunifredb@users.noreply.github.com>
2021-08-25 22:15:10 +0000

    Add files via upload

---
+++ HashTables1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashtables1;
+
+/**
+ *
+ * @author euni_freddd
+ */
+public class HashTables1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
